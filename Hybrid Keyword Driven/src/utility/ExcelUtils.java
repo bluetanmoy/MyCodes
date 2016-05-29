99a48567cf9d3a6de93f703a9907d4917348ea03
bluetanmoy <wirelesstanvir@gmail.com>
2016-05-29 06:52:42 -0400

    ExcelUtilitis written.

--- Hybrid Keyword Driven/src/utility/ExcelUtils.java
+++ Hybrid Keyword Driven/src/utility/ExcelUtils.java
@@ -1,5 +1,29 @@
 package utility;
 
-public class ExcelUtils {
+import java.io.FileInputStream;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtils {
+	public static XSSFWorkbook ExcelWBook;
+	public static XSSFSheet ExcelWSheet;
+	public static XSSFCell Cell;
+
+//This method is to set the File path and to open the Excel file
+//Pass Excel Path and SheetName as Arguments to this method
+public static void setExcelFile(String Path,String SheetName) throws Exception {
+        FileInputStream ExcelFile = new FileInputStream(Path);
+        ExcelWBook = new XSSFWorkbook(ExcelFile);
+        ExcelWSheet = ExcelWBook.getSheet(SheetName);
+       }
+
+//This method is to read the test data from the Excel cell
+//In this we are passing parameters/arguments as Row Num and Col Num
+public static String getCellData(int RowNum, int ColNum) throws Exception{
+	  Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
+      String CellData = Cell.getStringCellValue();
+      return CellData;
+	}
 }
